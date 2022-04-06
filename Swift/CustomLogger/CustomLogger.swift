f2c04732c0991ad02892688e4428102a99402dc8
Federico De Luca <fdeluca@orangeloops.com>
2022-04-06 16:36:47 -0300

    Swift CustomLogger improvements

--- Swift/CustomLogger/CustomLogger.swift
+++ Swift/CustomLogger/CustomLogger.swift
@@ -1,61 +1,96 @@
+import UIKit
+
 public class CustomLogger {
 
-  // MARK: Properties
+    // MARK: Class enums
 
-  public static let sharedInstance: CustomLogger = {
-    let instance = CustomLogger()
-    return instance
-  }()
-  private var log: String
-  private var lineNumber: Int
-
-  // MARK: Initialization
-
-  private init() {
-    self.log = ""
-    self.lineNumber = 0
-  }
-
-  // MARK: Public methods
-
-  public func add(text: String) {
-    lineNumber += 1
-    if !isFirstLine() {
-      self.log = self.log + "\n"
+    public enum LineIdentifier {
+        case counter, timeStamp
     }
-    var reviewedText: String
-    if text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-      reviewedText = "<Empty line>"
-    } else {
-      reviewedText = text
+
+    // MARK: Class properties
+
+    public static let sharedInstance: CustomLogger = {
+        let instance = CustomLogger()
+        return instance
+    }()
+
+    // MARK: Public properties
+
+    public var lineIdentifier: LineIdentifier
+
+    // MARK: Private properties
+
+    private var log: String
+    private var lineNumber: Int
+
+    // MARK: Initialization
+
+    private init() {
+        log = ""
+        lineIdentifier = .counter
+        lineNumber = 0
     }
-    self.log = self.log + "[" + String(lineNumber) + "] " + reviewedText
-  }
 
-  public func showAsAlertOn(viewController: UIViewController) {
-    let dialogMassage = UIAlertController(title: "<<<OL LOG>>>", message: self.log, preferredStyle: .alert)
-    let okAction = UIAlertAction(title: "Accept", style: .default)
-    dialogMassage.addAction(okAction)
-    viewController.present(dialogMassage, animated: true, completion: nil)
-  }
+    // MARK: Public methods
 
-  // MARK: Private methods
+    public func add(text: String) {
+        var reviewedText: String
+        if text.isEmptyAndWithoutWhiteSpaces() {
+            reviewedText = "<Empty line>"
+        } else {
+            reviewedText = text
+        }
+        if !isFirstLineInLog() {
+            log += "\n"
+        }
+        log += currentLineIdentifier() + " " + reviewedText
+    }
 
-  private func currentTimeTag() -> String {
-    let date = Date()
-    let calender = Calendar.current
-    let components = calender.dateComponents([.hour,.minute,.second,.nanosecond], from: date)
+    public func presentAsAlertOn(_ viewController: UIViewController) {
+        let dialogMassage = UIAlertController(title: "<<<CUSTOM LOG>>>", message: log, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Accept", style: .default)
+        dialogMassage.addAction(okAction)
+        viewController.present(dialogMassage, animated: true, completion: nil)
+    }
 
-    let hour = components.hour
-    let minute = components.minute
-    let second = components.second
-    let nanosecond = components.nanosecond
-    let currentTime = String(hour!) + ":" + String(minute!) + ":" +  String(second!) + ":" + String(nanosecond!)
+    // MARK: Private methods
 
-    return "[" + currentTime + "]"
-  }
+    private func currentLineIdentifier() -> String {
+        var currentIdentifier: String
 
-  private func isFirstLine() -> Bool {
-    return self.log.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
-  }
+        switch lineIdentifier {
+        case .counter:
+            currentIdentifier = String(lineNumber)
+            lineNumber += 1
+        case .timeStamp:
+            currentIdentifier = currentTimeTag()
+        }
+
+        return "[\(currentIdentifier)]"
+    }
+
+    private func currentTimeTag() -> String {
+        let date = Date()
+        let calender = Calendar.current
+        let components = calender.dateComponents([.hour, .minute, .second, .nanosecond], from: date)
+
+        let hour = components.hour
+        let minute = components.minute
+        let second = components.second
+        let nanosecond = components.nanosecond
+        let currentTime = String(hour!) + ":" + String(minute!) + ":" +  String(second!) + ":" + String(nanosecond!)
+
+        return currentTime
+    }
+
+    private func isFirstLineInLog() -> Bool {
+        return log.isEmptyAndWithoutWhiteSpaces()
+    }
+}
+
+fileprivate extension String {
+    func isEmptyAndWithoutWhiteSpaces() -> Bool {
+        return self.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    }
 }
