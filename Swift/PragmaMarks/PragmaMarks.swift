b00c648245fa8edad1f5cdb6a7eaeec1953fa663
Federico De Luca <fdeluca@orangeloops.com>
2022-04-07 11:02:25 -0300

    Swift Suggested Pragma marks clean up

--- Swift/PragmaMarks/PragmaMarks.swift
+++ Swift/PragmaMarks/PragmaMarks.swift
@@ -1,3 +1,4 @@
+// MARK: Enums
 // MARK: Class properties
 // MARK: Public properties
 // MARK: Private properties
@@ -5,19 +6,5 @@
 // MARK: Initialization
 // MARK: Class methods
 // MARK: Public methods
-// MARK: UITableViewCell methods
-// MARK: IBActions
-// MARK: UITableViewCell methods
-// MARK: UIViewController methods
-
-// MARK: UIViewController methods
-// MARK: BaseViewController methods
-// MARK: Styling methods
-// MARK: Event handlers
-// MARK: Notification handlers
-// MARK: Public methods
 // MARK: Private methods
-// MARK: LandingV2Presentable
-// MARK: BaseController Protocol Override
-// MARK: DimmerViewDelegate
-// MARK: FormBidingDelegate
+// MARK: IBActions
