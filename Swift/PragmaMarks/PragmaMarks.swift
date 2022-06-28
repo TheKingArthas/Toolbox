5958964244df24a36ae9d4f8b07f27fdafabaa61
Federico De Luca <federicoNdeluca@gmail.com>
2022-06-28 18:20:47 -0300

    Update PragmaMarks.swift

--- Swift/PragmaMarks/PragmaMarks.swift
+++ Swift/PragmaMarks/PragmaMarks.swift
@@ -1,12 +1,12 @@
-// MARK: Enums
-// MARK: Public class properties
-// MARK: Private class properties
-// MARK: Public properties
-// MARK: Private properties
-// MARK: IBOutlets
-// MARK: Initialization
-// MARK: Public class methods
-// MARK: Private class methods
-// MARK: Public methods
-// MARK: Private methods
-// MARK: IBActions
+// MARK: - Enums
+// MARK: - Public class properties
+// MARK: - Private class properties
+// MARK: - Public properties
+// MARK: - Private properties
+// MARK: - IBOutlets
+// MARK: - Initialization
+// MARK: - Public class methods
+// MARK: - Private class methods
+// MARK: - Public methods
+// MARK: - Private methods
+// MARK: - IBActions
